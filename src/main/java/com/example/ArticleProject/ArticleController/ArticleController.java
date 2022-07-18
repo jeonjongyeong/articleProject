83efdfef49a80515959e83efccd97907cabd0742
jkkjkhh <jkkjkhh@gmail.com>
2022-07-18 13:31:39 +0900

    article_detail 리스트 출력(오류)

--- src/main/java/com/example/ArticleProject/ArticleController/ArticleController.java
+++ src/main/java/com/example/ArticleProject/ArticleController/ArticleController.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -22,6 +23,13 @@ public class ArticleController {
         List<Article> articleList = articleService.getList();
         model.addAttribute("articleList", articleList);
         return "article_list";
+    }
+
+    @RequestMapping("/detail/{id}")
+    public String showDetail(Model model, @PathVariable("id") Integer id) {
+        Article article = articleService.getDetail(id);
+        model.addAttribute("article", article);
+        return "article_detail";
     }
 
 }
--- src/main/java/com/example/ArticleProject/service/ArticleService.java
+++ src/main/java/com/example/ArticleProject/service/ArticleService.java
@@ -2,10 +2,12 @@ package com.example.ArticleProject.service;
 
 import com.example.ArticleProject.Repository.ArticleRepository;
 import com.example.ArticleProject.dto.Article;
+import com.example.ArticleProject.util.DataNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ArticleService {
@@ -15,5 +17,14 @@ public class ArticleService {
 
     public List<Article> getList() {
         return articleRepository.findAll();
+    }
+
+    public Article getDetail(Integer id) {
+        Optional<Article> article = this.articleRepository.findById(id);
+        if (article.isPresent()) {
+            return article.get();
+        } else {
+            throw new DataNotFoundException("article not found");
+        }
     }
 }
